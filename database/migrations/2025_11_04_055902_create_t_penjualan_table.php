4dafde76aef46e422a78424e8ee37c99090c8fd5
alfitaraissaputri <alfitaraissa@gmail.com>
2025-11-04 13:05:23 +0700

    tugas webframework

--- database/migrations/2025_11_04_055902_create_t_penjualan_table.php
+++ database/migrations/2025_11_04_055902_create_t_penjualan_table.php
@@ -12,28 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_penjualan', function (Blueprint $table) {
-            $table->id();
-            $table->increments('penjualan_id');
-
-            // user_id (FK) int
-            // Pastikan ini di kedua file (t_penjualan dan t_stok)
-            $table->unsignedBigInteger('user_id');
-
-            // pembeli string(50)
-            $table->string('pembeli', 50);
-
-            // penjualan_kode string(20)
-            $table->string('penjualan_kode', 20);
-
-            // penjualan_tanggal datetime
-            $table->dateTime('penjualan_tanggal');
-            
-            // timestamps() dihapus karena tidak ada di gambar
-
-            // MENAMBAHKAN FOREIGN KEY
-            // (Saya asumsikan relasinya ke m_user(user_id))
-            $table->foreign('user_id')->references('user_id')->on('m_user');
+            $table->id('barang_id'); // ← cukup ini saja
+            $table->unsignedBigInteger('kategori_id');
+            $table->string('barang_kode', 10);
+            $table->string('barang_nama', 100);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
             $table->timestamps();
+
+            // Foreign key ke tabel m_kategori
+            $table->foreign('kategori_id')
+                  ->references('kategori_id')
+                  ->on('m_kategori')
+                  ->onDelete('cascade');
         });
     }
 
